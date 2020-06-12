ea486976b097ef22986600639201f5ec48f3a996
Gautham YS <gauthamys123@gmail.com>
2020-06-12 15:54:38 +0530

    ok

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
+import firebase
+from firebase import FirebaseApplication
+
+firebase = FirebaseApplication('https://malai-9065d.firebaseio.com/')
 app = Flask(__name__)
+
 @app.route('/')
 def one():
 	return render_template('index.html',title='one')
-@app.route('/two')
+
+@app.route('/two',methods=['GET','POST'])
 def two():
-	return render_template('two.html',title='two')
+	'''data = dict()
+	data['field 1'] = request.form['name']
+	data['field 2'] = request.form['name2']'''
+	data = firebase.get('/sensor1')
+	
+	return render_template('two.html',title='two',data=data)
+
+
 if __name__=='__main__':
 	app.run()
