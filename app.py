11b8d97c671919dfba108b4e7a904df97217ca10
Gautham YS <gauthamys123@gmail.com>
2020-06-12 16:36:15 +0530

    m

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask,render_template,request,redirect,url_for
-from firebaseapp import firebaseapp
+#from firebaseapp import firebaseapp
 app = Flask(__name__)
 
 @app.route('/')
 def one():
 	return render_template('index.html',title='one')
 
-@app.route('/two',methods=['GET','POST'])
+'''@app.route('/two',methods=['GET','POST'])
 def two():
 	data = dict()
 	data['field 1'] = request.form['name']
@@ -15,7 +15,7 @@ def two():
 	data2 = firebaseapp.get('/sensor1',None)
 	return data2
 	
-	#return render_template('two.html',title='two',data=data2)
+	#return render_template('two.html',title='two',data=data2)'''
 
 @app.route('/facts')
 def gay():
