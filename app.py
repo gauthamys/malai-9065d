a43389656b235b507ed5fd3a8fb41ae2d555260d
Gautham YS <gauthamys123@gmail.com>
2020-06-12 11:02:54 +0530

    'okay'

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def one():
-	return 'OK'
+	return render_template('index.html',title='one')
 @app.route('/two')
 def two():
-	return render_template('index.html')
+	return render_template('two.html',title='two')
 if __name__=='__main__':
 	app.run()
